bd17b400b1a9564c5153b81248f4fa88edf7cc7b
Chirag Goel <61325696+chirag-goel360@users.noreply.github.com>
2021-06-29 15:44:33 +0530

    complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,113 +1,208 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:google_ml_kit/google_ml_kit.dart';
+import 'package:image_picker/image_picker.dart';
 
 void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+  runApp(
+    MyHomePage(),
+  );
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title,}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  File _file;
+  String result = '';
+  List<Face> faces;
+  dynamic image;
+  ImagePicker imagePicker;
+  FaceDetector faceDetector;
 
-  void _incrementCounter() {
+  @override
+  void initState() {
+    super.initState();
+    imagePicker = ImagePicker();
+    faceDetector = GoogleMlKit.vision.faceDetector(
+      FaceDetectorOptions(
+        enableClassification: true,
+        minFaceSize: 0.1,
+        mode: FaceDetectorMode.accurate,
+      ),
+    );
+  }
+
+  doFaceDetection() async {
+    final inputImage = InputImage.fromFile(
+      _file,
+    );
+    faces = await faceDetector.processImage(
+      inputImage,
+    );
+    drawRectangleAroundFaces();
+    if (faces.length > 0) {
+      if (faces[0].smilingProbability > 0.5) {
+        result = "Smiling 😃";
+      } else {
+        result = "Serious 😑";
+      }
+    }
+  }
+
+  drawRectangleAroundFaces() async {
+    image = await _file.readAsBytes();
+    image = await decodeImageFromList(image);
+    setState(() {});
+  }
+
+  _imgFromCamera() async {
+    PickedFile image = await imagePicker.getImage(
+      source: ImageSource.camera,
+    );
+    _file = File(
+      image.path,
+    );
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      if (_file != null) {
+        doFaceDetection();
+      }
+    });
+  }
+
+  _imgFromGallery() async {
+    PickedFile image = await imagePicker.getImage(
+      source: ImageSource.gallery,
+    );
+    _file = File(
+      image.path,
+    );
+    setState(() {
+      if (_file != null) {
+        doFaceDetection();
+      }
     });
   }
 
   @override
+  void dispose() {
+    super.dispose();
+    faceDetector.close();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage(
+                'images/wall.jpg',
+              ),
+              fit: BoxFit.cover,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+          ),
+          child: Column(
+            children: [
+              Container(
+                margin: EdgeInsets.only(
+                  top: 70,
+                ),
+                child: Stack(
+                  children: [
+                    Center(
+                      child: TextButton(
+                        onPressed: _imgFromGallery,
+                        onLongPress: _imgFromCamera,
+                        child: Container(
+                          width: 350,
+                          height: 300,
+                          child: image != null
+                              ? Center(
+                                  child: FittedBox(
+                                    fit: BoxFit.fill,
+                                    child: Container(
+                                      width: image.width.toDouble(),
+                                      height: image.height.toDouble(),
+                                      child: CustomPaint(
+                                        painter: FacePainter(
+                                          rect: faces,
+                                          imageFile: image,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                )
+                              : Container(
+                                  color: Colors.black,
+                                  child: Icon(
+                                    Icons.camera_alt,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(
+                  top: 20,
+                ),
+                child: Text(
+                  '$result',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 36,
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+}
+
+class FacePainter extends CustomPainter {
+  List<Face> rect;
+  var imageFile;
+  FacePainter({
+    @required this.rect,
+    @required this.imageFile,
+  });
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    if (imageFile != null) {
+      canvas.drawImage(
+        imageFile,
+        Offset.zero,
+        Paint(),
+      );
+    }
+    Paint paint = Paint();
+    paint.color = Colors.red;
+    paint.style = PaintingStyle.stroke;
+    paint.strokeWidth = 16;
+    if (rect != null) {
+      for (Face rectangle in rect) {
+        canvas.drawRect(
+          rectangle.boundingBox,
+          paint,
+        );
+      }
+    }
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return true;
   }
 }
